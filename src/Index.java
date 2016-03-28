0e12614e8cec0bc1031438ade0fb94d6cf8bc631
salilkansal <salilkansal.92@gmail.com>
2016-03-27 19:54:23 -0500

    Initial commit

---
+++ src/Index.java
@@ -0,0 +1,10 @@
+/**
+ * @author Salil Kansal
+ * @author Twinkle Sharma
+ * @author Sujit Sajja
+ */
+
+public interface Index {
+    public void putIndex(int index);
+    public int getIndex();
+}
